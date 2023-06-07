0516b33a9c7e929e1dee5bc1258d02fe325e1234
Ge <55168978+GeSamaras@users.noreply.github.com>
2023-06-07 07:09:16 -0300

    Update and rename 2nd-largest.py to algo-theory-problems.py

--- algo-theory-problems.py
+++ algo-theory-problems.py
@@ -40,3 +40,29 @@ def find_second_largest(arr):
 arr = [10, 5, 7, 2, 9, 1, 15, 8]
 second_largest = find_second_largest(arr)
 print("Second largest number:", second_largest)
+
+#Q2: You are a given a unimodal array of n distinct elements, 
+#    meaning that its entries are in increasing order up until its maximum element,
+#    after which its elements are in decreasing order. Give an algorithm to compute the maximum element that runs in O(log n) time.
+
+def find_maximum(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        mid = left + (right - left) // 2
+
+        if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+            return arr[mid]
+        elif arr[mid - 1] > arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    # Error condition: Array is not unimodal
+    return None
+
+# Example usage
+arr = [2, 5, 9, 12, 15, 7, 3, 1]
+maximum = find_maximum(arr)
+print("Maximum element:", maximum)
